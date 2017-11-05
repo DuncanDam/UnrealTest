7cedfac1bffa9dfd368ae78d66c26a8d435c8ea0
Duncan Dam <damducanh87@gmail.com>
2017-11-05 20:17:24 +0700

    Create a new basic c++ project and add some basic Unreal Engine free assets to be used in game

---
+++ Source/UnrealBomberman/UnrealBombermanGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealBombermanGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALBOMBERMAN_API AUnrealBombermanGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
